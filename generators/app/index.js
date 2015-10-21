d93931ccf34b5fadffdb01bdfa8d6a992aedeed3
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-21 19:00:51 +0200

    simplify it

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,9 +2,6 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-  constructor: function () {
-    generators.Base.apply(this, arguments);
-  },
   writing: function () {
     this.spawnCommandSync('git', ['init']);
   }
